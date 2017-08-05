387b7d04a7a7429f7c8e8f100d43ec78c9584df5
Laspimon <casperlehmann@gmail.com>
2017-08-06 00:08:56 +0200

    Handle messages from web-interface

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,29 @@ def index():
 @app.route('/messages', methods=['GET'])
 def list_messages():
     return render_template('/messages/index.html')
+
+@app.route('/new', methods=['GET'])
+def new_order_form():
+    return render_template('messages/new-order.html')
+
+@app.route('/new', methods=['POST'])
+def receive_message():
+    message = request.form.get('message', '')
+    drink = {
+        'g&t': 'Gin & Tonic',
+        'espresso-martini': 'Espresso Martini',
+        'negroni': 'Negroni',
+        'beer': 'Beer',
+        'other': request.form.get('other')
+    }.get(request.form.get('drink'))
+    if drink is None:
+        return (
+            'Something\'s wrong with your order, '
+            'perhaps you meant to select "Other".',
+            400)
+    #save_massage()
+    #broadcast_message()
+    return ('drink', 204)
 
 if __name__ == '__main__':
     app.run()
--- tests/test_app.py
+++ tests/test_app.py
@@ -33,5 +33,32 @@ class ServerTestCase(unittest.TestCase):
             self.assertEqual(res.status_code, 200)
             assert b'<h1>hello world!</h1>' in res.data
 
+    def test_new_order_form_renders_choices(self):
+        with server.app.test_request_context():
+            index = url_for('new_order_form')
+        res = self.client.get(index)
+        self.assertEqual(res.status_code, 200)
+        assert b'Gin & Tonic' in res.data
+        assert b'Espresso Martini' in res.data
+        assert b'Negroni' in res.data
+        assert b'Beer' in res.data
+        assert b'Other' in res.data
+
+    def test_receive_message_returns_204_on_success(self):
+        data = {'drink': 'g&t', 'message': 'Make it strong.'}
+        with server.app.test_request_context():
+            res = self.client.post('/new', data=data)
+        self.assertEqual(res.status_code, 204)
+        self.assertEqual(res.data, b'')
+
+    def test_receive_message_returns_400_on_no_drink_selection(self):
+        with server.app.test_request_context():
+            response_data, status_code  = server.receive_message()
+        self.assertEqual(
+            response_data,
+            'Something\'s wrong with your order, perhaps you meant to '
+            'select "Other".',)
+        self.assertEqual(status_code, 400)
+
 if __name__ == '__main__':
     unittest.main()
