3e4f0b81265fd4cac3a36a04f0b19cc60f8824af
Laspimon <casperlehmann@gmail.com>
2017-08-05 19:16:35 +0200

    Utilize Flask's own testing infrastructure to establish test_request_context.
    This means that we do not need to mock the live server environment, so we can eliminate dependency on external testing tools: flask_testing.
    
    Logging limitation too can then be removed as it only served to declutter the
    output when establishing live connection.

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'Hello World'
+    return redirect('/messages', code=302)
+
+@app.route('/messages', methods=['GET'])
+def list_messages():
+    return render_template('/messages/index.html')
 
 if __name__ == '__main__':
     app.run()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,29 +1,36 @@
-import flask_testing
-import logging
 import unittest
-import urllib
+
+from flask import url_for
+from urllib.parse import urlparse
 
 import server
 
-logging.disable(logging.CRITICAL)
+class ServerTestCase(unittest.TestCase):
 
-class TestIndex(unittest.TestCase):
+    def setUp(self):
+        #self.db_fd, server.app.config['DATABASE'] = tempfile.mkstemp()
+        server.app.testing = True
+        self.client = server.app.test_client()
+        #with server.app.app_context():
+        #    server.init_db()
 
-    def test_index_returns_greeting(self):
-        self.assertEqual(server.index(), 'Hello World')
+    def tearDown(self):
+        #os.close(self.db_fd)
+        #os.unlink(server.app.config['DATABASE'])
+        pass
 
-class TestLiveIndex(flask_testing.LiveServerTestCase):
+    def test_index_redirects_with_302_to_messages(self):
+        with server.app.test_request_context():
+            index = url_for('index')
+            res = self.client.get(index, follow_redirects=False)
+            self.assertEqual(res.status_code, 302)
+            self.assertEqual(urlparse(res.location).path, '/messages')
 
-    def create_app(self):
-        app = server.Flask(server.__name__)
-        app = server.app
-        app.config['TESTING'] = True
-        app.config['LIVE_SERVERPORT'] = 0
-        return app
-
-    def test_server_awake(self):
-        res = urllib.request.urlopen(self.get_server_url())
-        self.assertEqual(res.code, 200)
+    def test_messages_contains_greeting(self):
+        with server.app.test_request_context():
+            index = url_for('index')
+            res = self.client.get(index, follow_redirects=True)
+            assert b'<h1>hello world!</h1>' in res.data
 
 if __name__ == '__main__':
     unittest.main()
