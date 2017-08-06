430ef389fe25b452c127b95d9269cc549e1e6be2
Laspimon <casperlehmann@gmail.com>
2017-08-07 01:50:35 +0200

    PEP8

--- server.py
+++ server.py
@@ -11,7 +11,8 @@ ctime_format = "%a %b %d %H:%M:%S %Y"
 logger = logging.getLogger('input_log')
 logger.setLevel(logging.INFO)
 file_handler = logging.FileHandler('transact.log')
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
